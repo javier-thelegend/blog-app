304b21884421c6ce2255aaba7a5ce51d0d97784b
Javier Cornejo <javier.thelegend@gmail.com>
2022-08-22 17:11:06 -0600

    Pagination and Sorting

--- controller/posts.js
+++ controller/posts.js
@@ -1,16 +1,34 @@
 const Posts = require('../model/posts');
 
+const getTotalsOfPagination = (totalElements, pageSize, pageNo) => {
+    const totalPages = Math.ceil(totalElements / pageSize)
+    return { totalElements, totalPages, lastOne: (totalPages - 1) == pageNo }
+}
+
 module.exports.findAllPosts = (req, res, next) => {
     //Pagination Parameters
-    const { pageNo, pageSize } = req.query
+    const pageNo = parseInt(req.query.pageNo);
+    const pageSize = parseInt(req.query.pageSize);
+    const sortBy = req.query.sortBy || "id";    //By default sort by id
+    const sortDir = req.query.sortDir.toLowerCase() === "asc" ? "asc" : "desc";
+
     //Implement Pagination
-    Posts.findAll({
-        order: [['id', 'asc']],
-        limit: pageSize || 10,          //By default get 10 records
-        offset: (pageNo * pageSize) || 0 //By default get page 0
+    Posts.findAndCountAll({
+        order: [[sortBy, sortDir]],
+        limit: pageSize || 10,              //By default get 10 records
+        offset: pageNo * pageSize || 0      //By default get page 0
     })
         .then((result) => {
-            res.status(200).json({ valid: true, data: result })
+            const { totalElements, totalPages, lastOne } = getTotalsOfPagination(result.count, pageSize, pageNo)
+            var response = {
+                content: result.rows,
+                pageNo,
+                pageSize,
+                totalElements,
+                totalPages,
+                lastOne
+            }
+            res.status(200).json({ valid: true, data: response })
         })
         .catch((e) => res.status(400).json({ valid: false, message: e }))
 }
--- db/config.js
+++ db/config.js
@@ -3,11 +3,11 @@ dotenv.config();
 
 module.exports = {
     postgre: {
-        user: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        host: process.env.PGHOST,
-        database: process.env.PGDATABASE,
-        port: process.env.PGPORT
+        user: process.env.POSTGRE_USER,
+        password: process.env.POSTGRE_PASS,
+        host: process.env.POSTGRE_HOST,
+        database: process.env.POSTGRE_DATABASE,
+        port: process.env.POSTGRE_PORT
     }//,
     // server: {
     //     port: 3800
